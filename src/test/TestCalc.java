ed3993fcaa4c7f3c681689df6ed6c3b822535916
fraserbyfleet <f.byfleet@gmail.com>
2019-08-21 22:47:25 +1200

    Tutorial 4 first commit 15078889...

---
+++ src/test/TestCalc.java
@@ -0,0 +1,22 @@
+package Tutorial 4;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions*;
+import org.junit.jupiter.api.Test;
+
+class TestCalc {
+		
+
+	    @Test
+	    pulic void Test_add() {Assertions.assertEquals(5, Calc.add(3,5))};
+	    
+	    
+	    @Test
+	    pulic void Test_add() {Assertions.assertEquals(5, Calc.add(10,5))};
+
+
+	    
+
+}
